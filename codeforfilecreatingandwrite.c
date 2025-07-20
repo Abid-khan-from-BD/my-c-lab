140639fda5dc74f63f0cf88d074e128deb6a1608
Abid-Khan-from-BD <abid20002017@gmail.com>
2025-07-20 22:40:39 +0600

    delete some line

--- codeforfilecreatingandwrite.c
+++ codeforfilecreatingandwrite.c
@@ -5,7 +5,6 @@ int main()
     file = fopen("file.txt", "w");
     fprintf(file,"Hello World");
     fclose(file);
-    printf("I add some text");
-    printf("Some text added by Abid Khan");
+    
     return 0;
 }
